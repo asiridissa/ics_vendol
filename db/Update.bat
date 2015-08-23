744ca79bccc47a383463e3124f55d4bf81e0ab2f
dumindawije <duminda_wijeya@yahoo.com>
2015-08-23 14:23:43 +0530

    Duminda

--- db/Update.bat
+++ db/Update.bat
@@ -1,8 +1,22 @@
 @echo off
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Content in the local machine will be override. Are you sure [Y/N] ?&
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+echo Update will proceed...&
+rar a db.rar *.* -x*.bat -x*.exe
+echo This will take few seconds..
 svn cleanup
 svn update
 set PATH=C:\Program Files\WinRar;%PATH%
 e:
 :cd ics_vendol\db
 unrar e -o+ db
+echo Update completed...&
 pause
+
+:END
+endlocal
+
+
+
