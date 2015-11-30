366fd2abf27501140d049f896be27fd0dbca1bd7
dumindawije <duminda_wijeya@yahoo.com>
2015-11-30 10:31:41 +0530

    DaySaleReport Summmary Quantity/Amount tree change

--- bin/print/Summary.bat
+++ bin/print/Summary.bat
@@ -1,6 +1,6 @@
 @echo off
 e:
-del /F E:\ICS_Shaw\bin\print\Summary.xlsx
-copy E:\ICS_Shaw\bin\print\ori\Summary.xlsx E:\ICS_Shaw\bin\print\Summary.xlsx
+del /F E:\ICS\bin\print\Summary.xlsx
+copy E:\ICS\bin\print\ori\Summary.xlsx E:\ICS\bin\print\Summary.xlsx
 exit
 @echo off
